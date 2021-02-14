98d986cf52429afea79e689f756ee9e3eebc1c45
Samuel <sagarma_94@hotmail.com>
2021-02-14 01:05:33 +0100

    Docker for CICD

---
+++ docker/test.sh
@@ -0,0 +1,4 @@
+curl -x http://172.17.0.1:3128  -L https://google.es
+curl -x http://172.17.0.1:3128  -L https://testtodeny.com
+curl -x http://172.17.0.1:3128  -L https://violence.com
+curl -x http://172.17.0.1:3128  -L https://pornpage.com
